f2d0a616b398d58721ee41460108bfc1a775ac01
Roopesh V S <txtmeroopesh@gmail.com>
2020-08-08 20:07:27 +0530

    Heroku Support #2

--- ActivityPeriodAPIv1/settings.py
+++ ActivityPeriodAPIv1/settings.py
@@ -12,7 +12,6 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 
 from pathlib import Path
 import django_heroku
-django_heroku.settings(locals())
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
@@ -122,3 +121,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+django_heroku.settings(locals())
